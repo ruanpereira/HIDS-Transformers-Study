f7d665ef8f89c5bd50b86b1a877e53671aaf6fe8
aktsu_ruan <black_aktsu@proton.me>
2023-03-27 19:54:37 -0300

    27/03/23

--- HandsOnPractice.py
+++ HandsOnPractice.py
@@ -4,4 +4,4 @@ import pandas as pd
 
 #Carrega os dados
 ddos_database = pd.read_csv("/run/media/aktsu_user/Mem_linux/Databases/01-12/Syn.csv", low_memory=False)
-print(ddos_database.info())
+ddos_database.head()
